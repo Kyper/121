c77054fea0c95f5832b82cd85aedcee80632b884
Vova Korginzkyy <kyper32@ya.ru>
2013-05-20 09:49:20 +0300

    Create 121.cpp

---
+++ 121.cpp
@@ -0,0 +1,40 @@
+// 121.cpp: определяет точку входа для консольного приложения.
+//
+#include "stdafx.h"
+#include "join.h"
+#include "car.h"
+
+
+using namespace std;
+
+int main()
+{
+   string TypeCar; 
+   int PriceCar;
+   int WayCar;
+   Car myCar; 
+   
+
+   cout << myCar.getTypeCar() << endl;
+
+   
+   cout << "\nPlease enter the car type:" << endl;
+   cin>>TypeCar; 
+   myCar.setTypeCar( TypeCar );
+ 
+   cout << "\nPlease enter the car price:" << endl;
+   cin>>PriceCar; 
+   myCar.setPriceCar( PriceCar );
+
+   cout << "\nPlease enter the car way:" << endl;
+   cin>>WayCar; 
+   myCar.setWayCar( WayCar );
+   
+   myCar.displayMessage();
+
+   //---------------------------------------------------
+   cout<<"Length of Type: "<<TypeCar.length()<<" symbols"<<endl;
+   cout << endl;
+   getch();
+} 
+
